b8f7e46fcc97d03454d4fec5d1d71f521fafdcfe
unknown <you@example.com>
2023-02-08 20:32:15 +0530

    hello

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,23 @@ package com.driver;
 
 public class Main
 {
+        public static class  A
+        {
+            public String meth()
+            {
+                return "Invoking method from class A";
+            }
+        }
+    public static class B extends A
+    {
+        @Override
+        public String meth()
+        {
+            return "Method is overridden in Extendend class B";
+
+        }
+    }
+
     public static void main(String[] args)
     {
         B b=new B();
@@ -9,21 +26,10 @@ public class Main
 
         B b1=new B();
         b1.meth();
+    }
 
-    }
+
 }
-class  A
-{
-    public  String  meth()
-     {
-         return "Invoking method from class A";
-     }
-}
-public static class B extends A
-{
-    @Override
-    public String meth()
-    {
-            return "Method is overridden in Extendend class B";
-    }
-}
+
+
+
